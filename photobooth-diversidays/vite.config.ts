ab321c51b16bd9e008e46234be5fd302e0383221
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-06-15 17:12:40 +0200

    chore: adding stuff to backend

--- photobooth-diversidays/vite.config.ts
+++ photobooth-diversidays/vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.IS_DEV !== 'true' ? './' : '/',
+  build: {
+    outDir: 'app/build',
+  },
   plugins: [vue()]
 })
