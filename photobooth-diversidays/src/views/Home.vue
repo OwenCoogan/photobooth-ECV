7c2c72ab21c743160151cd7fb84a053260c92f79
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-06-16 16:14:09 +0200

    chore: fixing tailwind config

--- photobooth-diversidays/src/views/Home.vue
+++ photobooth-diversidays/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container max-w-sm">
     <UniqueLogo />
   </div>
 </template>
---
+++ photobooth-diversidays/src/views/Webcam.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Webcam/>
+  </div>
+</template>
+
+<script>
+import Webcam from '../components/Webcam.vue'
+export default {
+  components: {
+    Webcam
+  }
+
+}
+</script>
+
+<style>
+
+</style>
