2cfeabcc0105e0748751950b0710c8485ea68054
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-06-16 15:57:41 +0200

    chore: adding router config

---
+++ photobooth-diversidays/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <UniqueLogo />
+  </div>
+</template>
+
+<script>
+  import UniqueLogo from '../components/SVG/UniqueLogo.vue'
+export default {
+  components: {
+    UniqueLogo
+  }
+}
+</script>
